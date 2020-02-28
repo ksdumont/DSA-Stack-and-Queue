223f77f6369249bcf52efaf5d2297d6adfa99419
ksdumont <ksdumont@gmail.com>
2020-02-27 17:16:25 -0800

    sort

--- drills.js
+++ drills.js
@@ -62,3 +62,105 @@ console.log(is_palindrome("A man, a plan, a canal: Panama"));
 console.log(is_palindrome("1001"));
 console.log(is_palindrome("Tauhida"));
 
+//4. Matching parentheses in an expression
+
+const parser = function(string) {
+  const parserStack = new Stack();
+  let singleQuoteActive = false;
+  let doubleQuoteActive = false;
+  const pairs = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  };
+  for (let character in string) {
+    if (string[character] === "'" && doubleQuoteActive === false) {
+      singleQuoteActive = !singleQuoteActive;
+    }
+    if (string[character] === '"' && singleQuoteActive === false) {
+      doubleQuoteActive = !doubleQuoteActive;
+    }
+    if (
+      (string[character] === "(" ||
+        string[character] === "[" ||
+        string[character] === "{") &&
+      singleQuoteActive === false &&
+      doubleQuoteActive === false
+    ) {
+      parserStack.push({
+        character: string[character],
+        location: character
+      });
+    }
+    if (
+      (string[character] === ")" ||
+        string[character] === "]" ||
+        string[character] === "}") &&
+      singleQuoteActive === false &&
+      doubleQuoteActive === false
+    ) {
+      if (isEmpty(parserStack)) {
+        const expectedChar = Object.keys(pairs).find(
+          key => pairs[key] === string[character]
+        );
+        return `Missing opening '${expectedChar}' before the closing '${string[character]}' at location ${character}!`;
+      }
+      let previousStackNode = parserStack.pop();
+      if (string[character] !== pairs[previousStackNode.character]) {
+        return `Expecting a '${
+          pairs[previousStackNode.character]
+        }' but found a '${string[character]}' at location ${character}!`;
+      }
+    }
+  }
+  if (isEmpty(parserStack)) {
+    return "All is well with your brackets and nesting. Well done!";
+  } else {
+    let currNode = parserStack.top;
+    let missingCloser;
+    while (currNode !== null) {
+      missingCloser = parserStack.pop();
+      console.log(
+        `Missing closing '${
+          pairs[missingCloser.character]
+        }' after the opening '${missingCloser.character}' at location ${
+          missingCloser.location
+        }!`
+      );
+      currNode = currNode.next;
+    }
+    return false;
+  }
+  return true;
+};
+console.log(parser("([{M'a({[r'k}])"));
+
+//5. Sort stack
+
+const sortStack = function(stack) {
+  const sortedStack = new Stack();
+  if (stack.top === null) {
+    return "Empty Stack";
+  }
+  let temp;
+  while (stack.top) {
+    temp = stack.pop();
+    while (sortedStack.top && sortedStack.top.data < temp) {
+      stack.push(sortedStack.pop());
+    }
+    sortedStack.push(temp);
+  }
+  return display(sortedStack);
+};
+const stackToSort = new Stack();
+stackToSort.push(7);
+stackToSort.push(3);
+stackToSort.push(9);
+stackToSort.push(1);
+stackToSort.push(4);
+console.log(display(stackToSort));
+console.log(sortStack(stackToSort));
+
+//6. Create a queue using Singly linked list
+
+
